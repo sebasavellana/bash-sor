247b1eebba198b0cb0bf9fd113fae178cab27055
sebasavellana <35118660+sebasavellana@users.noreply.github.com>
2021-10-06 10:47:57 +0200

    Update ej04.sh

--- ej04.sh
+++ ej04.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Autor: Sebas Avellana
 # Fecha: 5/10/21
-# Versión: 1.0
-# Funcionalidad: Pida al usuario el nombre de la carpeta a crear dentro del directorio donde se ejecuta el script.
+# Versión: 1.3
+# Funcionalidad: Pide al usuario el nombre de la carpeta a crear dentro del directorio donde se ejecuta el script.
 # Debe funcionar en cualquier equipo de la clase SIN MODIFICACIÓN
 # Deberá mostrar el directorio creado con sus permisos
 # Creará también un fichero de nombre sor.txt dentro del directorio que se crea
